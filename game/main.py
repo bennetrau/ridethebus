6542fc325428db6a2bdb162b26bd9d627331a7c6
1104-Rau-Bennet <98195903+1104-Rau-Bennet@users.noreply.github.com>
2025-05-06 19:51:39 -0700

    test

--- game/main.py
+++ game/main.py
@@ -2,7 +2,7 @@ from game.game import RideTheBus
 
 
 def main():
-    print("Ride the Bus the game!... v1.3\n")
+    print("Ride the Bus the game!... v1.4\n")
 
     while True:
         play_game()
